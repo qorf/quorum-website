a0353cd5ec6a3d3c56965ab79072616d6417b8c6
kluthe <kluthe@unlv.nevada.edu>
2022-06-02 16:18:20 -0500

    Updated charts kitchen sink page: fixed the arrow/bracket navigation, added additional factor/separation settings, fixed traversal by ordered bar values for each type

--- QuorumWebsite/html/script/quorumCharts-beta.js
+++ QuorumWebsite/html/script/quorumCharts-beta.js
@@ -4,7 +4,47 @@
 	 //ENTER
 	 if (event.keyCode == 13) {
 		console.log("CHART AREA ENTER");
-		this.firstChild.focus();
+		this.getElementsByTagName('g')[0].focus();
+		flag = true;
+	 }
+	 
+	  if (flag) {
+		event.stopPropagation();
+		event.preventDefault();
+	  }
+ }
+
+ function seriesKeydown() {
+	 flag = false;
+	 console.log("KEYDOWN EVENT");
+	 //ENTER
+	 if (event.keyCode == 13) {
+		console.log("CATEGORY ENTER");
+		this.getElementsByTagName('g')[0].focus();
+		flag = true;
+	 }
+	 //ESCAPE
+	 else if (event.keyCode == 27) {
+		console.log("CATEGORY ESCAPE");
+		this.parentNode.focus();
+		flag = true;
+	 }
+	 //RIGHT
+	 else if(event.keyCode == 39){
+		console.log("CATEGORY RIGHT");
+		if(this.nextSibling)
+			this.nextSibling.focus();
+		else
+			this.parentNode.getElementsByTagName('g')[0].focus();
+		flag = true;
+	 }
+	 //LEFT
+	 else if(event.keyCode == 37){
+		console.log("CATEGORY LEFT");
+		if(this.previousSibling && this.previousSibling.tagName == 'g')
+			this.previousSibling.focus();
+		else
+			this.parentNode.lastChild.focus();
 		flag = true;
 	 }
 	 
@@ -41,7 +81,7 @@
 	 //LEFT
 	 else if(event.keyCode == 37){
 		console.log("CATEGORY LEFT");
-		if(this.previousSibling)
+		if(this.previousSibling && this.previousSibling.tagName == 'g')
 			this.previousSibling.focus();
 		else
 			this.parentNode.lastChild.focus();
@@ -94,7 +134,7 @@
 		if(typeof quorum_chart_value_order != "undefined" && quorum_chart_value_order != null) {
 			var value_order = quorum_chart_value_order[svgId];
 			
-			if (this.parentNode.classList.contains("quorum-chart-category-list"))
+			if (this.parentNode.classList.contains("quorum-chart-category-list") || this.parentNode.classList.contains("quorum-chart-series-list"))
 				value_order = value_order[this.parentNode.id];
 				
 			if (value_order[this.id] != null) {
@@ -126,7 +166,7 @@
 		if(typeof quorum_chart_value_order != "undefined" && quorum_chart_value_order != null) {
 			var value_order = quorum_chart_value_order[svgId];
 			
-			if (this.parentNode.classList.contains("quorum-chart-category-list"))
+			if (this.parentNode.classList.contains("quorum-chart-category-list") || this.parentNode.classList.contains("quorum-chart-series-list"))
 				value_order = value_order[this.parentNode.id];
 			
 			if (value_order != null && value_order[this.id] != null) {
@@ -182,6 +222,11 @@ window.addEventListener('load', function () {
   document.querySelectorAll('.quorum-chart-area').forEach(item => {
 	  console.log("Adding chart area event handler");
 	  item.addEventListener('keydown', chartAreaKeydown, false)
+  })
+  
+  document.querySelectorAll('.quorum-chart-series-list').forEach(item => {
+	  console.log("Adding series event handler");
+	  item.addEventListener('keydown', seriesKeydown, false)
   })
   
   document.querySelectorAll('.quorum-chart-category-list').forEach(item => {
