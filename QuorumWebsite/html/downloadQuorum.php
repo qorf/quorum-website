752dd651b941d542db185487516d727d532cfe9b
Andreas Stefik <stefik@gmail.com>
2022-06-24 13:43:27 -0700

    Finished a new version.

--- QuorumWebsite/html/downloadQuorum.php
+++ QuorumWebsite/html/downloadQuorum.php
@@ -100,7 +100,7 @@
 
     $studioVersionMajor = 4;
     $studioVersionMinor = 0;
-    $studioVersionSubminor = 0;
+    $studioVersionSubminor = 2;
 
     $studioVersionName = "" . $studioVersionMajor . "." . $studioVersionMinor . "." . $studioVersionSubminor; 
     
