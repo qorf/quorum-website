f7ac0525e726f781b0ffc5fb3fc15850bf4ea28b
Andreas Stefik <stefik@gmail.com>
2022-06-23 17:50:44 -0700

    Updated PHP number.

--- QuorumWebsite/html/downloadQuorum.php
+++ QuorumWebsite/html/downloadQuorum.php
@@ -100,7 +100,7 @@
 
     $studioVersionMajor = 4;
     $studioVersionMinor = 0;
-    $studioVersionSubminor = 0;
+    $studioVersionSubminor = 1;
 
     $studioVersionName = "" . $studioVersionMajor . "." . $studioVersionMinor . "." . $studioVersionSubminor; 
     
