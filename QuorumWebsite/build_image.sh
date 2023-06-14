d6fa4a45cacc4c67609260ba1c99ad39b6d1663a
Andreas Stefik <stefik@gmail.com>
2023-06-14 13:42:13 -0700

    Updated version numbers.

--- QuorumWebsite/build_image.sh
+++ QuorumWebsite/build_image.sh
@@ -1,2 +1,2 @@
 docker buildx create --name quorum-database-server-builder --node quorum-database-server-builder0 --use
-docker buildx build --no-cache -f Dockerfile.quorum-database-server.build -t andreasstefik/quorum-database-server:1.4.10 . --platform linux/amd64,linux/arm64 --push
+docker buildx build --no-cache -f Dockerfile.quorum-database-server.build -t andreasstefik/quorum-database-server:1.4.11 . --platform linux/amd64,linux/arm64 --push
