8a3bf4dd25f1426f3435f749bbb0a96e0092f0f2
Andreas Stefik <stefik@gmail.com>
2023-09-08 10:48:59 -0700

    Changed version numbers.

--- QuorumWebsite/html/embed/embed-quorum.js
+++ QuorumWebsite/html/embed/embed-quorum.js
@@ -34,7 +34,7 @@ var hightlightCode = function() {
 
 LoadPrism();
 LoadScript("https://quorumlanguage.com/script/load.js");
-LoadScript("https://quorumlanguage.com/script/QuorumStandardLibrary.js?version=10.6.0");
+LoadScript("https://quorumlanguage.com/script/QuorumStandardLibrary.js?version=11.1.0");
 
 
 
